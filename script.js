21ba759e9c457df792dee1dc9104e4b40980427e
ConnerKT <connerthompson121@gmail.com>
2023-03-23 13:23:21 -0500

    JS Addition

--- script.js
+++ script.js
@@ -1,2 +1,80 @@
-// Template
-t
+// Setting Event Listeners to the buttons
+
+let zerobutton = document.getElementById("zero").addEventListener("click", function(event){
+    let a = zero;
+});
+let onebutton = document.getElementById("one").addEventListener("click", function(event){
+    let a = one;
+});
+let twobutton = document.getElementById("two").addEventListener("click", function(event){
+    let a = two;
+});
+let threebutton = document.getElementById("three").addEventListener("click", function(event){
+    let a = three;
+});
+let fourbutton = document.getElementById("four").addEventListener("click", function(event){
+    let a = four;
+});
+let fivebutton = document.getElementById("five").addEventListener("click", function(event){
+    let a = five;
+});
+let sixbutton = document.getElementById("six").addEventListener("click", function(event){
+    let a = six;
+});
+let sevenbutton = document.getElementById("seven").addEventListener("click", function(event){
+    let a = seven;
+});
+let eightbutton = document.getElementById("eight").addEventListener("click", function(event){
+    let a = eight;
+});
+let ninebutton = document.getElementById("nine").addEventListener("click", function(event){
+    let a = nine;
+});
+
+// Setting Event Listenters for the operators/main componenets
+let equalbutton = document.getElementById("equal").addEventListener("click", function(event){
+    // Equal the result 
+});
+let multiplybutton = document.getElementById("multiply").addEventListener("click");
+let plusbutton = document.getElementById("plus").addEventListener("click", function(event){
+    container =+ parseInt(a);
+    a = 0;
+});
+let subtractbutton = document.getElementById("subtract").addEventListener("click");
+let clearbutton = document.getElementById("clear").addEventListener("click", function(event){
+    let total = 0;
+    let a = "0";
+    let b = "0";
+});
+
+//Setting Event Listenters for the complex components
+let decimalbutton = document.getElementById("decimal").addEventListener("click");
+let percentage = document.getElementById("percentage").addEventListener("click");
+let plusnegativebutton = document.getElementById("negative").addEventListener("click");
+
+
+// Setting Variables that set our placeholder variable to the number (string)
+let zero = "0";
+let one = "1";
+let two = "2";
+let three = "3";
+let four = "4";
+let five = "5";
+let six = "6";
+let seven = "7";
+let eight = "8";
+let nine = "9";
+
+// Our containers for a number
+let a = 0;
+let b = 0;
+let container = 0;
+let total = 0;
+
+
+if (equalbutton === true){
+    total
+}
+
+
+
