e46d8b30776d0093b6b5c30bb6b72c2fed5622a4
ConnerKT <connerthompson121@gmail.com>
2023-03-27 19:25:31 -0500

    Fixing Value additon

--- script.js
+++ script.js
@@ -34,70 +34,85 @@ let total = 0;
 
 let totalHTML = document.getElementById("total");
 totalHTML.innerHTML = "0"
-let zerobutton = document.getElementById("zero").
-addEventListener("click", function(event){
-    let a = zero;
-    totalHTML.innerHTML = '0';
+let zerobutton = document.getElementById("zero").addEventListener("click", function(){
+    a = zero;
+    totalHTML.innerHTML += '0';
 });
-let onebutton = document.getElementById("one").addEventListener("click", function(event){
-    let a = one;
-    totalHTML.innerHTML = "1"
+let onebutton = document.getElementById("one").addEventListener("click", function(){
+    a = one;
+    totalHTML.innerHTML += "1"
 });
-let twobutton = document.getElementById("two").addEventListener("click", function(event){
-    let a = two;
-    totalHTML.innerHTML = "2"
-});
-let threebutton = document.getElementById("three").addEventListener("click", function(event){
-    let a = three;
-    totalHTML.innerHTML = "3"
-});
-let fourbutton = document.getElementById("four").addEventListener("click", function(event){
-    let a = four;
-    totalHTML.innerHTML = "4"
-});
-let fivebutton = document.getElementById("five").addEventListener("click", function(event){
-    let a = five;
-    totalHTML.innerHTML = "5"
-});
-let sixbutton = document.getElementById("six").addEventListener("click", function(event){
-    let a = six;
-    totalHTML.innerHTML = "6"
-});
-let sevenbutton = document.getElementById("seven").addEventListener("click", function(event){
-    let a = seven;
-    totalHTML.innerHTML = "7"
-});
-let eightbutton = document.getElementById("eight").addEventListener("click", function(event){
-    let a = eight;
-    totalHTML.innerHTML = "8"
-});
-let ninebutton = document.getElementById("nine").addEventListener("click", function(event){
-    let a = nine;
-    totalHTML.innerHTML = "9"
+let twobutton = document.getElementById("two").addEventListener("click", function(){
+    a = two;
+    totalHTML.innerHTML += "2"
     console.log(a)
+});
+let threebutton = document.getElementById("three").addEventListener("click", function(){
+    a = three;
+    totalHTML.innerHTML += "3"
+});
+let fourbutton = document.getElementById("four").addEventListener("click", function(){
+    a = four;
+    totalHTML.innerHTML += "4"
+});
+let fivebutton = document.getElementById("five").addEventListener("click", function(){
+    a = five;
+    totalHTML.innerHTML += "5"
+});
+let sixbutton = document.getElementById("six").addEventListener("click", function(){
+    a = six;
+    totalHTML.innerHTML += "6"
+});
+let sevenbutton = document.getElementById("seven").addEventListener("click", function(){
+    a = seven;
+    totalHTML.innerHTML += "7"
+});
+let eightbutton = document.getElementById("eight").addEventListener("click", function(){
+    a = eight;
+    totalHTML.innerHTML += "8"
+});
+let ninebutton = document.getElementById("nine").addEventListener("click", function(){
+    a = nine;
+    totalHTML.innerHTML += "9"
 });
 
 // Setting Event Listenters for the operators/main componenets
-let equalbutton = document.getElementById("equal").addEventListener("click", function(event){
+let equalbutton = document.getElementById("equal").addEventListener("click", function(){
+    console.log("total", total)
+    // let parseVar = parseInt(a)
+    // total += parseVar;
     totalHTML.innerHTML = total;
+    console.log(total)
+    parseVar = 0;
 });
 
-let multiplybutton = document.getElementById("multiply").addEventListener("click", function(event){
+let multiplybutton = document.getElementById("multiply").addEventListener("click", function(){
+    totalHTML.innerHTML = "0";
 
 });
-let plusbutton = document.getElementById("plus").addEventListener("click", function(event){
-    total =+ parseInt(a);
-    a= "0";
+let plusbutton = document.getElementById("plus").addEventListener("click", function(){
+    totalHTML.innerHTML = "0";
+    let parseVar = parseInt(a)
+    total += parseVar;
+    a = 0;
+    parseVar = 0;
+  
 });
-let subtractbutton = document.getElementById("subtract").addEventListener("click", function(event){
+let subtractbutton = document.getElementById("subtract").addEventListener("click", function(){
+    totalHTML.innerHTML = "0";
 
 });
+let divisionbutton = document.getElementById("divide").addEventListener("click", function(){
+    totalHTML.innerHTML = "0";
+
+})
 // If A/C is clicked, clear total, and also associate variables
 
-let clearbutton = document.getElementById("clear").addEventListener("click", function(event){
-    let total = 0;
-    let a = "0";
-    let b = "0";
+let clearbutton = document.getElementById("clear").addEventListener("click", function(){
+    total = 0;
+    a = "0";
+    b = "0";
+    totalHTML.innerHTML = "0"
 });
 
 //Setting Event Listenters for the complex components
